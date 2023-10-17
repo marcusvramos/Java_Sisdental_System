82f05fcea82ceb09392c564669287aeb945f6238
marcus-rubcube <marcus.vinicius@rubcube.com>
2023-10-17 20:44:37 -0300

    feat: adding an precedimento dal

--- src/main/java/br/fipp/sisdentalfx/Testes.java
+++ src/main/java/br/fipp/sisdentalfx/Testes.java
@@ -2,10 +2,8 @@ package br.fipp.sisdentalfx;
 
 import br.fipp.sisdentalfx.db.dals.MaterialDAL;
 import br.fipp.sisdentalfx.db.dals.PessoaDAL;
-import br.fipp.sisdentalfx.db.entidades.Dentista;
-import br.fipp.sisdentalfx.db.entidades.Material;
-import br.fipp.sisdentalfx.db.entidades.Paciente;
-import br.fipp.sisdentalfx.db.entidades.Pessoa;
+import br.fipp.sisdentalfx.db.dals.ProcedimentoDAL;
+import br.fipp.sisdentalfx.db.entidades.*;
 import br.fipp.sisdentalfx.db.util.DB;
 
 import java.util.List;
@@ -19,7 +17,7 @@ public class Testes {
 //            for (Material material : lista)
 //                System.out.println(material);
 
-            PessoaDAL dal=new PessoaDAL();
+            //PessoaDAL dal=new PessoaDAL();
 //            if(!dal.gravar(new Dentista("Teodoro",123,"18996807124","joelton@email.com"))) {
 //                DB.getCon().getMensagemErro();
 //            }
@@ -37,11 +35,18 @@ public class Testes {
 //            )) {
 //                DB.getCon().getMensagemErro();
 //            }
-            List<Pessoa> lista = dal.get("", new Paciente());
-            //System.out.println(dal.get(1, new Paciente()).getNome());
-            for(Pessoa p : lista){
-                System.out.println(((Paciente)p).getCpf());
+//            List<Pessoa> lista = dal.get("", new Paciente());
+//            //System.out.println(dal.get(1, new Paciente()).getNome());
+//            for(Pessoa p : lista){
+//                System.out.println(((Paciente)p).getCpf());
+//            }
+            ProcedimentoDAL dal = new ProcedimentoDAL();
+            if(!dal.gravar(new Procedimento("Clareamento Dental", 50, 250))){
+                DB.getCon().getMensagemErro();
             }
+            List<Procedimento> lista = dal.get("");
+            for(Procedimento p : lista)
+                System.out.println(p.getDescricao());
 
         }
         else
